6f3b931a63e3fae87d3d715888738384a72b1208
evanchansky <40421385+evanchansky@users.noreply.github.com>
2018-07-03 09:59:13 +0800

    Update getUserEmail.py

--- getUserEmail.py
+++ getUserEmail.py
@@ -1,1 +1,3 @@
-
+SenderEmail = input("Enter the senders email address \n it is used to log in to the server so it must be a gmail account \n Email: ")
+SenderPassword = input("Enter the password to the above email address: ")
+AdminEmail = input("Enter the email address you would like the alert to be sent to: ")
