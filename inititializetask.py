72537ccf2f8c7ea81b854b0c8e122428d1cb4f98
evanchansky <40421385+evanchansky@users.noreply.github.com>
2018-07-03 09:38:50 +0800

    Update inititializetask.py

--- inititializetask.py
+++ inititializetask.py
@@ -1,4 +1,4 @@
 import os
 
-os.system("schtasks /Create /SC MINUTE /TN requesthtml TR/ C:\\Users\\90499\\dist\\requesthtml.exe") 
+os.system("schtasks /Create /SC MINUTE /TN requesthtml /TR C:\\Users\\90499\\dist\\requesthtml.exe") 
 # the path off the executable file will have to be changed for where the file is saved
