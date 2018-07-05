da438a3bcfcf19e21115e1b09e1f03c5efc9fbc5
evanchansky <40421385+evanchansky@users.noreply.github.com>
2018-07-05 14:56:22 +0800

    Update getUserEmail.py

--- getUserEmail.py
+++ getUserEmail.py
@@ -1,4 +1,4 @@
 SenderEmail = input("Enter the senders email address \n it is used to log in to the server so it must be a gmail account \n Email: ")
 SenderPassword = input("Enter the password to the above email address: ")
 AdminEmail = input("Enter the email address you would like the alert to be sent to: ")
-ListURL = input("Enter the list of URLs you would like to check: ")
+ListURL = input("Enter the URL you would like to check: ")
