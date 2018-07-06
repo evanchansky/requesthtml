e79ac469dcc0e1042c091cdec51e9b568ec74466
evanchansky <40421385+evanchansky@users.noreply.github.com>
2018-07-06 14:34:46 +0800

    Delete getUserEmail.py

--- getUserEmail.py
+++
@@ -1,4 +0,0 @@
-SenderEmail = input("Enter the senders email address \n it is used to log in to the server so it must be a gmail account \n Email: ")
-SenderPassword = input("Enter the password to the above email address: ")
-AdminEmail = input("Enter the email address you would like the alert to be sent to: ")
-ListURL = input("Enter the URL you would like to check: ")
