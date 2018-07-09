16f6c00d84c30a00f42dfbc55993ee9af6b31d11
evanchansky <40421385+evanchansky@users.noreply.github.com>
2018-07-09 09:52:53 +0800

    Update inititializetask.py

--- inititializetask.py
+++ inititializetask.py
@@ -2,7 +2,7 @@ import os
 
 os.system("pipenv run pyinstaller --onefile --noconsole -y requesthtml.py")		
 
-exePathName = "C:\\Users\\90499\\ServerCheck\\requesthtml.exe" 									# Finds path the newly created .exe file
+exePathName = "C:\\Users\\90499\\dist\\requesthtml.exe" 									# Finds path the newly created .exe file
 commandSchtask = "schtasks /Create /SC MINUTE /TN requesthtml /TR " + exePathName 			# Builds cmd line for creating the repeated task
 print(commandSchtask)
 os.system(commandSchtask) 																	# Creates the task
