34f51c84e90a4d771bb87c08357b0287aa80c0ed
evanchansky <40421385+evanchansky@users.noreply.github.com>
2018-08-10 11:15:14 +0800

    Update inititializetask.py

--- inititializetask.py
+++ inititializetask.py
@@ -1,20 +1,23 @@
 import os
 
+os.system("pip install requests")
+os.system("pip install pyinstaller")
+
 pathName = os.path.abspath(os.path.dirname(__file__))
 specPathName = os.path.join(pathName, "requesthtml.spec")
 exePathName = os.path.join(pathName, "requesthtml.exe")
 pathFileName = os.path.join(pathName, "path.txt")
 
 f = open(pathFileName, "w") 
-f.write(pathName)
+f.write(pathName) 				# writes path of folder to a fuile that can be used by other scripts
 f.close()
 
-cmdPacakageExe = "pipenv run pyinstaller -F --distpath=%s %s"	% (pathName, specPathName)
+cmdPacakageExe = "pyinstaller -F --distpath=%s %s"	% (pathName, specPathName)
 print(cmdPacakageExe)
 os.system(cmdPacakageExe)
 
-commandSchtask = "schtasks /Create /SC MINUTE /MO 5 /TN requesthtml /TR %s" % (exePathName) 			# Builds cmd line for creating the repeated task
+commandSchtask = "schtasks /Create /SC MINUTE /MO 5 /TN requesthtml /TR %s" % (exePathName)		# Builds cmd line for creating the repeated task
 print(commandSchtask)
-os.system(commandSchtask) 	
+os.system(commandSchtask) 	# Creates the task
 
-wait = input("All set! Press enter to continue: ")				# Creates the tasky
+wait = input("All set! Press enter to continue: ")				
